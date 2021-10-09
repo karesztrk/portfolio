8232c4fba1dab03264a0f3d2f91cd8e4dd355e0e
Károly Török <torok.karoly.krisztian@gmail.com>
2021-10-09 17:08:02 +0200

    Chemaxon

--- src/components/sections/Experience/Experience.tsx
+++ src/components/sections/Experience/Experience.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 import {
   Container,
   Divider,
   SectionHeader,
   Tabs,
   Tab,
-} from 'components/common';
-import { Wrapper, Content, Details } from './styles';
-import useInViewAnimation from 'hooks/useInViewAnimation';
+} from "components/common";
+import { Wrapper, Content, Details } from "./styles";
+import useInViewAnimation from "hooks/useInViewAnimation";
 
 const leftSide = {
   hidden: {
@@ -19,7 +19,7 @@ const leftSide = {
     x: 0,
     transition: {
       staggerChildren: 0.2,
-      ease: 'easeOut',
+      ease: "easeOut",
       duration: 1,
     },
   },
@@ -38,33 +38,75 @@ const header = {
 
 export const Experience = () => {
   const { ref, animation } = useInViewAnimation();
+  const lastChangeYear = new Date("2021").getFullYear();
+  const currentYear = new Date().getFullYear();
+  const currentYears = currentYear - lastChangeYear + 1;
   return (
-    <Wrapper ref={ref} as={Container} id='experience'>
-      <SectionHeader initial='hidden' animate={animation} variants={header}>
+    <Wrapper ref={ref} as={Container} id="experience">
+      <SectionHeader initial="hidden" animate={animation} variants={header}>
         <h2>Experience</h2>
         <Divider />
       </SectionHeader>
-      <Details initial='hidden' animate={animation} variants={leftSide}>
-        <Tabs activeTab='minero' animation={animation}>
-          <Tab tabKey='minero' title='Minero IT' buttonGrow={5}>
+      <Details initial="hidden" animate={animation} variants={leftSide}>
+        <Tabs activeTab="chemaxon" animation={animation}>
+          <Tab tabKey="chemaxon" title="ChemAxon" buttonGrow={currentYears}>
+            <Content>
+              <h4>
+                Full-stack developer @&nbsp;
+                <a
+                  href="https://chemaxon.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  ChemAxon
+                </a>
+              </h4>
+              <h4>2021 - Present</h4>
+              <div>
+                <p>
+                  My current workplace is ChemAxon. The company is offering
+                  several mature products to the Biotechnology and
+                  Pharmaceutical industries. Over the years, their products
+                  became industry standards. By joining to this family, I'm now
+                  a - so called - ChemAxionian 👾.
+                </p>
+                <ul>
+                  <li>
+                    Opportunity to plan and implement a product Front-End
+                    redesign fully autonomously
+                  </li>
+                  <li>
+                    New way of thinking: I'm entering the Chemistry industry
+                  </li>
+                  <li>
+                    Efficient team and business collaboration to serve quality
+                    products to the customers
+                  </li>
+                  <li>Customer and consulting support</li>
+                </ul>
+              </div>
+            </Content>
+          </Tab>
+          <Tab tabKey="minero" title="Minero IT" buttonGrow={6}>
             <Content>
               <h4>
                 Software developer @&nbsp;
                 <a
-                  href='http://www.minero-it.hu/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="http://www.minero-it.hu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Minero IT
                 </a>
               </h4>
-              <h4>2015 - Present</h4>
+              <h4>2015 - 2021</h4>
               <div>
                 <p>
-                  I currently work at an outsourcing company called Minero. Here
-                  I've learn a lot. This is not just about about the enormous
-                  technical stuff but the way of delivering products to a
-                  customer in a mature way.
+                  Minero holds a specal place in my hearth. It is - my first
+                  employer in the private sector - a software consulting
+                  company. Here I've learnt a lot. This is not just about about
+                  the enormous technical stuff but the way of delivering
+                  products to a customer in a mature way.
                 </p>
                 <ul>
                   <li>
@@ -85,14 +127,14 @@ export const Experience = () => {
               </div>
             </Content>
           </Tab>
-          <Tab tabKey='aensys' title='AENSys' buttonGrow={3}>
+          <Tab tabKey="aensys" title="AENSys" buttonGrow={3}>
             <Content>
               <h4>
                 Lead developer @&nbsp;
                 <a
-                  href='https://aensys.hu/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://aensys.hu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   AENSys
                 </a>
@@ -113,14 +155,14 @@ export const Experience = () => {
               </div>
             </Content>
           </Tab>
-          <Tab tabKey='university' title='University of Szeged' buttonGrow={3}>
+          <Tab tabKey="university" title="University of Szeged" buttonGrow={3}>
             <Content>
               <h4>
                 Experienced developer @&nbsp;
                 <a
-                  href='https://www.inf.u-szeged.hu/'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://www.inf.u-szeged.hu/"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Institute of Informatics
                 </a>
