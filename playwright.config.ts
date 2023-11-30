47a675e0a4c06c398608db4e60e3aa680bf99166
Károly Török <torok.karoly.krisztian@gmail.com>
2023-11-30 16:03:25 +0100

    Github CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   use: {
     baseURL: "http://localhost:4321/",
   },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
   webServer: {
-    command: "pnpm run preview",
+    command: "pnpm preview",
     url: "http://localhost:4321/",
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
