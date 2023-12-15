e79ffeb79aad669d517898b69d7ddb6cded737a9
Károly Török <torok.karoly.krisztian@gmail.com>
2023-12-15 19:10:57 +0100

    Post about Content Compression

--- src/util/blog.util.ts
+++ src/util/blog.util.ts
@@ -1,3 +1,9 @@
+export const removeMarkdown = (body: string) => {
+  // - Remove special characters
+  // - Remove parathesis
+  return body.replace(/([*_`~\[\]]|\([^)]*\))/g, "");
+};
+
 export const formatTitle = (id: string) => {
   const parts = id.split(".");
   return parts[0];
@@ -5,7 +11,8 @@ export const formatTitle = (id: string) => {
 
 export const formatExcerpt = (body: string) => {
   const paragraphs = body.split(/\n/);
-  return paragraphs.find((p) => p.trim() !== "");
+  const paragraph = paragraphs.find((p) => p.trim() !== "");
+  return paragraph ? removeMarkdown(paragraph) : undefined;
 };
 
 const formatter = new Intl.DateTimeFormat("en-US", {
