ec9638c34482727b4159c2b9e5e5817258ab9007
Károly Török <torok.karoly.krisztian@gmail.com>
2024-01-18 14:37:38 +0100

    CSS optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import nuDiscoTheme from "./nu-disco-dark.color-theme.json";
 
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
