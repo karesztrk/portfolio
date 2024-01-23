0defb8b0f9e15633340c1a9ea6a2efeb42d7a1ec
Károly Török <torok.karoly.krisztian@gmail.com>
2024-01-23 14:38:03 +0100

    Reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import nuDiscoTheme from "./nu-disco-dark.color-theme.json";
 import netlify from "@astrojs/netlify";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import mdx from "@astrojs/mdx";
 
@@ -9,6 +10,7 @@ export default defineConfig({
     shikiConfig: {
       theme: nuDiscoTheme,
     },
+    remarkPlugins: [remarkReadingTime],
   },
   integrations: [mdx()],
   output: "hybrid",
