b7c2f1df6f29be42c5f4368143f01e4f80794702
Károly Török <torok.karoly.krisztian@gmail.com>
2024-05-08 17:58:23 +0200

    Mind-map renamed to Tooltips

--- src/test/tooltips.test.ts
+++ src/test/tooltips.test.ts
@@ -1,18 +1,18 @@
 import { test, expect } from "./fixtures";
 
-test.describe("Mind map page", () => {
-  test("Page elements present", async ({ mindMap, page }) => {
-    await expect(page.getByRole("heading", { name: "Mind map" })).toBeVisible();
+test.describe("Tooltips page", () => {
+  test("Page elements present", async ({ tooltips, page }) => {
+    await expect(page.getByRole("heading", { name: "Tooltips" })).toBeVisible();
     await expect(page.getByRole("button", { name: "Search" })).toBeVisible();
   });
 
-  test("Search entry", async ({ mindMap, page }) => {
-    await mindMap.openSearch();
+  test("Search entry", async ({ tooltips, page }) => {
+    await tooltips.openSearch();
 
-    await expect(mindMap.searchDialog()).toBeVisible();
+    await expect(tooltips.searchDialog()).toBeVisible();
     await page.locator("#search-input").fill("html");
 
-    const dialog = mindMap.searchDialog();
+    const dialog = tooltips.searchDialog();
     // Fist button is the close button, the Second is the first inside the menu
     const entryButton = dialog.locator("button").locator("nth=1");
     const entryText = await entryButton.textContent();
@@ -20,7 +20,7 @@ test.describe("Mind map page", () => {
     expect(entryText).not.toBeNull();
     await entryButton.click();
 
-    await expect(mindMap.searchDialog()).not.toBeVisible();
+    await expect(tooltips.searchDialog()).not.toBeVisible();
     if (entryText) {
       await expect(
         page.getByRole("article").getByRole("article").getByText(entryText),
@@ -29,14 +29,14 @@ test.describe("Mind map page", () => {
   });
 
   test("Search dialog can be closed with Keyboard", async ({
-    mindMap,
+    tooltips,
     page,
   }) => {
-    await mindMap.openSearch();
-    await expect(mindMap.searchDialog()).toBeVisible();
+    await tooltips.openSearch();
+    await expect(tooltips.searchDialog()).toBeVisible();
 
     await page.locator("id=search-dialog").press("Escape");
 
-    await expect(mindMap.searchDialog()).not.toBeVisible();
+    await expect(tooltips.searchDialog()).not.toBeVisible();
   });
 });
