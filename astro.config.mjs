b79b6a61be27c75efe59dd9e0b9ba73a5bbc75e6
Károly Török <torok.karoly.krisztian@gmail.com>
2024-05-30 18:32:32 +0200

    Scroll to top

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import nuDiscoTheme from "./nu-disco-dark.color-theme.json";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 import { browserslistToTargets } from "lightningcss";
 import browserslist from "browserslist";
-
+import rehypeScrollToTop from "@benjc/rehype-scroll-to-top";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
@@ -12,6 +12,18 @@ export default defineConfig({
       theme: nuDiscoTheme,
     },
     remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypeScrollToTop,
+        {
+          topLink: { disabled: true },
+          bottomLink: {
+            text: "Back to top",
+            ariaLabel: "Back to top",
+          },
+        },
+      ],
+    ],
   },
   integrations: [mdx()],
   vite: {
