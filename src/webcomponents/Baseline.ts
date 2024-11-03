d191063b99b4c91678376feba49fdea5c5abecad
Károly Török <torok.karoly.krisztian@gmail.com>
2024-11-03 16:53:27 +0100

    Baseline frontmatter

--- src/webcomponents/Baseline.ts
+++ src/webcomponents/Baseline.ts
@@ -1,6 +1,6 @@
-import { BaselineStatus, BaselineIcon } from "@karesztrk/baseline-status";
+import { BonelessStatus, BaselineIcon } from "@karesztrk/baseline-status";
 
-class Baseline extends BaselineStatus {
+class Baseline extends BonelessStatus {
   static {
     this.register("baseline-icon", BaselineIcon);
     this.register("baseline-status", Baseline);
