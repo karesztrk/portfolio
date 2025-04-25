c569f7f366d688e6f1e9aa6e0e71a5489416ff84
Károly Török <torok.karoly.krisztian@gmail.com>
2025-04-25 12:55:29 +0200

    Experimental Fonts API

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,101 @@ export default defineConfig({
   prefetch: true,
   experimental: {
     clientPrerender: true,
+    fonts: [
+      {
+        provider: "local",
+        name: "Recursive",
+        cssVariable: "--font-recursive",
+        variants: [
+          {
+            /* Latin */
+            /* Variable with MONO & CASL axis values */
+            weight: 400,
+            style: "normal",
+            src: [
+              {
+                url: "./src/assets/fonts/Recursive-Normal-Var.woff2",
+                tech: "variations",
+              },
+            ],
+            display: "fallback",
+
+            // src:
+            //   local("Recursive"),
+            //   url("/fonts/Recursive-Normal-Var.woff2") format(woff2) tech(variations);
+            unicodeRange: [
+              "U+0000-00FF",
+              "U+0131",
+              "U+0152-0153",
+              "U+02BB-02BC",
+              "U+02C6",
+              "U+02DA",
+              "U+02DC",
+              "U+0304",
+              "U+0308",
+              "U+0329",
+              "U+2000-206F",
+              "U+2074",
+              "U+20AC",
+              "U+2122",
+              "U+2191",
+              "U+2193",
+              "U+2212",
+              "U+2215",
+              "U+FEFF",
+              "U+FFFD",
+            ],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: [
+              {
+                url: "./src/assets/fonts/Recursive-Bold.woff2",
+                tech: "variations",
+              },
+            ],
+            display: "fallback",
+            unicodeRange: [
+              "U+0000-00FF",
+              "U+0131",
+              "U+0152-0153",
+              "U+02BB-02BC",
+              "U+02C6",
+              "U+02DA",
+              "U+02DC",
+              "U+0304",
+              "U+0308",
+              "U+0329",
+              "U+2000-206F",
+              "U+2074",
+              "U+20AC",
+              "U+2122",
+              "U+2191",
+              "U+2193",
+              "U+2212",
+              "U+2215",
+              "U+FEFF",
+              "U+FFFD",
+            ],
+          },
+        ],
+      },
+      {
+        provider: "local",
+        name: "Departure",
+        cssVariable: "--font-departure",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            display: "fallback",
+            //   size-adjust: 105%;
+            src: ["./src/assets/fonts/DepartureMono-Regular.woff2"],
+          },
+        ],
+      },
+    ],
   },
   routes: {
     "/": "/hello",
