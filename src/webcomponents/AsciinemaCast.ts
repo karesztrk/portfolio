45f7a5e3c2b4318a6e4c7a9dc211d9b1d28cff31
Károly Török <ktorok@chemaxon.com>
2025-06-20 14:19:47 +0200

    Rehype Asciinema plugin

--- src/webcomponents/AsciinemaCast.ts
+++ src/webcomponents/AsciinemaCast.ts
@@ -1,16 +1,36 @@
 import { LightElement } from "@karesztrk/webcomponent-base";
 import * as AsciinemaPlayer from "asciinema-player";
 
+/**
+ * Asciinema cast player webcomponent.
+ */
 class AsciinemaCast extends LightElement {
   static {
     this.register("asciinema-cast", AsciinemaCast);
   }
 
   /**
-   * Returns a href prop value.
+   * Returns the name of the cast file (e.g. `hello-world.cast` -> `hello-world`).
    */
   get fileName(): string | null {
-    return this.getAttribute("file");
+    const source = this.src;
+    if (!source) {
+      return null;
+    }
+    const file = source.split("/").pop();
+    if (!file) {
+      return null;
+    }
+
+    // remove extension
+    return file.split(".")[0];
+  }
+
+  /**
+   * Returns a src prop value.
+   */
+  get src(): string | null {
+    return this.getAttribute("src");
   }
 
   /**
@@ -26,12 +46,13 @@ class AsciinemaCast extends LightElement {
    * Once connected load the cast lazily.
    */
   connectedCallback() {
-    if (!this.fileName) {
+    const name = this.fileName;
+    if (!name) {
       return;
     }
 
     // Only the file name can be dynmic according to the Vite spec
-    import(`../content/Blog/Casts/${this.fileName}.cast`)
+    import(`../content/Blog/Casts/${name}.cast`)
       .then((value) => {
         AsciinemaPlayer.create(value.default, this, this.settings);
       })
